5d1d61eae3abbff70ba9fd3bae0dc1d0fab179a8
Rajesh Goriga <v-gorraj@microsoft.com>
2022-01-12 18:45:17 +0530

    Add dummy data

--- src/Sample.jsx
+++ src/Sample.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 export default class Sample extends React.Component {
-    constructor(props){
-      super(props)
-      this.state={name:'Rajesh'}
-    }
-    render() {
-      return (
-        <div>
-          <h2>Hello this is {this.state.name}</h2>
-          <button onClick={this.click}>click</button>
-          {this.props.val}
-        </div>
-      );
-    }
-    click = ()=>{
-      this.setState({name:"Name changed"})
-    }
+  constructor(props) {
+    super(props);
+    this.state = { name: "Rajesh", data: {} };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => this.setState({ data: json }));
+  }
+
+  render() {
+    console.log('data', this.state.data)
+    return (
+      <div>
+        <h2>Hello this is {this.state.name}</h2>
+        <button onClick={this.click}>click</button>
+        {this.props.val}
+      </div>
+    );
+  }
+  click = () => {
+    this.setState({ name: "Name changed" });
+  };
+}
